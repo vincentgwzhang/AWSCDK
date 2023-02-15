7d49c27cf7bee3f018a8a32f1ce3393762ebcf53
Vincent Zhang <vincentzhang@outlook.es>
2023-02-15 13:45:56 +0100

    project init

--- README.md
+++ README.md
@@ -12,3 +12,40 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+
+
+```bash
+# 建立工程目录
+mkdir komapp
+cd komapp
+# 初始化 cdk 项目工程
+cdk init app --language typescript
+# cdk init app --language javascript
+# cdk init app --language python
+# cdk init app --language java
+# cdk init app --language csharp
+# cdk init app --language go
+# 编辑工程堆栈文件
+nano lib/komapp-stack.ts
+# 编译工程检测语法错误
+npm run build
+# 列出应用程序中的堆栈
+cdk ls
+# 检测潜在问题
+cdk doctor
+# 合成 AWS CloudFormation 模板
+cdk synth
+# 设置 bootstrap 引导程序, 部署 CDKToolkit 堆栈到 AWS CloudFormation 环境(v2需要)
+# -> https://docs.aws.amazon.com/zh_cn/cdk/v2/guide/bootstrapping.html
+# cdk bootstrap aws://ACCOUNT-NUMBER/REGION
+cdk bootstrap aws://232100206199/eu-west-2
+# 部署堆栈
+cdk deploy
+# 查看堆栈信息
+cdk metadata AwscdkStack
+# 修改程序，查看更改
+cdk diff
+# 销毁资源
+cdk destroy
+```
--- lib/awscdk-stack.ts
+++ lib/awscdk-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as path from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwscdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 定义 Lambda 函数
+    const authLambda = new cdk.aws_lambda.Function(this, "authLambda", {
+      runtime: cdk.aws_lambda.Runtime.NODEJS_14_X,
+      handler: "auth.handler",
+      code: cdk.aws_lambda.Code.fromAsset(path.join(__dirname, "../services/lambda")),
+    });
+    const memberLambda = new cdk.aws_lambda.Function(this, "memberLambda", {
+      runtime: cdk.aws_lambda.Runtime.NODEJS_14_X,
+      handler: "member.handler",
+      code: cdk.aws_lambda.Code.fromAsset(path.join(__dirname, "../services/lambda")),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwscdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 定义 API Gateway
+    const restApi = new cdk.aws_apigateway.RestApi(this, "restApi");
+
+    // 路径映射资源
+    const auth = restApi.root.addResource("auth");
+    auth.addMethod("GET", new cdk.aws_apigateway.LambdaIntegration(authLambda));
+    const member = restApi.root.addResource("member");
+    member.addMethod("GET", new cdk.aws_apigateway.LambdaIntegration(memberLambda));
   }
 }
